688e49ac5ac4f558573e68090676f69c2bd671ee
Ronan Kitten <ronank.dev@gmail.com>
2024-07-05 19:58:47 +0700

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/web-portfolio/",
+	base: "./web-portfolio/",
 	plugins: [react()],
 	resolve: {
 		alias: {
